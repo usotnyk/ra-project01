89f7dc365403c68cecd3f95a2e69f796b2b9f00b
Julia Sotnyk <usotnyk@gmail.com>
2017-04-18 20:58:41 -0400

    added regexp for email validation in html

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,16 @@
-console.log("hello");
+$( document ).ready(function() {
+    addEventListeners();
+});
+
+var addEventListeners = function() {
+  addSubscribeButtonClickListener();
+}
+
+var addSubscribeButtonClickListener = function() {
+  var subscribeButton = $("#subscribeButton");
+  subscribeButton.bind("click", onSubscribeButtonClick);
+}
+
+var onSubscribeButtonClick = function() {
+  console.log("subscribe button clicked!");
+}
